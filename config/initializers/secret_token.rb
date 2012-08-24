51427d9a6434099c544b5194736f46050f37eb76
Zekarias Araya <zekarias@Zekariass-MacBook-Pro.local>
2012-08-24 15:41:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tewelde::Application.config.secret_token = '84f460fdf4f7c64a69ad38d041d23564880e58cf18f7e075d3297a1a30d7dba2da7058b9c9579d03351833d9a44104c170cb88e29dd50163a68540f6943c1e08'
